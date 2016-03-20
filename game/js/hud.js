3ee1cb2e1a41032ec3dc16290d7285c66c223479
IsaacPYoung <iyoung@uwaterloo.ca>
2016-03-20 12:58:42 -0300

    New font
    - changed font to be more thematic

--- game/js/hud.js
+++ game/js/hud.js
@@ -1,17 +1,27 @@
 // the hud
+
+WebFontConfig = {
+
+    google: {
+      families: ['Cinzel']
+    }
+
+};
+
 var hud = {
 
 	score: null,
 	score_text: null,
 
 	preload: function() {
-
+		game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 	},
 
 	create: function() {
 		hud.score = 0;
 		hud.score_text = game.add.text(16, 16, '0', {
-			fontSize: '32px',
+			font: 'Cinzel',
+			fontSize: '48px',
 			fill: '#fff',
 		});
 	},
